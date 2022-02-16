64c58e85b3df19731980338fb53aedc5c86b1c9b
Brewalan <sbpk.pro@gmail.com>
2022-02-16 18:18:15 +0100

    Style work on header layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let x =document.querySelector(".topLinks");
+    if (x.getElementsByClassName.display === "block") {
+        x.getElementsByClassName.display = "none";
+    } else {
+        x.getElementsByClassName.display = "block";
+    }
+}
